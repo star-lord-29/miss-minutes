e2ac847ca7677198d8f912e6927d2e4e93ca0d28
star-lord-29 <108613741+star-lord-29@users.noreply.github.com>
2022-07-27 13:42:15 +0530

    Update direct_link_generator.py

--- bot/helper/mirror_utils/download_utils/direct_link_generator.py
+++ bot/helper/mirror_utils/download_utils/direct_link_generator.py
@@ -70,8 +70,8 @@ def direct_link_generator(link: str):
         return krakenfiles(link)
     elif is_gdtot_link(link):
         return gdtot(link)
-    elif any(x in link for x in fmed_list):
-        return fembed(link)
+    elif 'wetransfer.com' in link:
+        return wetransfer(link)
     elif any(x in link for x in ['sbembed.com', 'watchsb.com', 'streamsb.net', 'sbplay.org']):
         return sbembed(link)
     else:
@@ -208,6 +208,36 @@ def onedrive(link: str) -> str:
         raise DirectDownloadLinkException("ERROR: Unauthorized link, the link may be private")
     dl_link = resp.next.url
     return dl_link
+  
+import urllib.parse
+import cloudscraper
+import requests
+
+
+def wetransfer(url):
+    if url.startswith("https://we.tl/"):
+        r = requests.head(url, allow_redirects=True)
+        url = r.url
+    recipient_id = None
+    params = urllib.parse.urlparse(url).path.split("/")[2:]
+    if len(params) == 2:
+        transfer_id, security_hash = params
+    elif len(params) == 3:
+        transfer_id, recipient_id, security_hash = params
+    else:
+        return None
+    j = {"intent": "entire_transfer","security_hash": security_hash,}
+    if recipient_id:
+        j["recipient_id"] = recipient_id
+    s = cloudscraper.create_scraper(allow_brotli=False)
+    r = s.get("https://wetransfer.com/")
+    m = re.search('name="csrf-token" content="([^"]+)"', r.text)
+    s.headers.update({"x-csrf-token": m[1], "x-requested-with": "XMLHttpRequest"})
+    r = s.post(f"https://wetransfer.com/api/v4/transfers/{transfer_id}/download", json=j)
+    j = r.json()
+    dl_url = j["direct_link"]
+    dl_url = dl_url.replace(" ", "%20")
+    return dl_url  
 
 def pixeldrain(url: str) -> str:
     """ Based on https://github.com/yash-dk/TorToolkit-Telegram """
